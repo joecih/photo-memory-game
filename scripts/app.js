101493842f402f40547adee90d9fd5423c672d0a
joecih <joecih@gmail.com>
2016-03-03 17:09:41 -0500

    Overhaul to photo-memor-game
    Added in clean versions of index.html and related style sheet. Images
    added and moved scripting to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,22 @@
-$(document).ready( function() {
-var d = new Date();
-console.log(document.getElementById("date").innerHTML = d.toString());
 
-});
+function flip(element) {
+    if (( element.children[0].style.display == "block") || (element.children[0].style.display == "")) {
+        element.children[0].style.display = "none";
+        element.children[1].style.display = "block";
+    } else {
+        element.children[0].style.display = "block";
+        element.children[1].style.display = "none";
+    }
+
+};
+
+function restart() {
+    var back = document.getElementsByClassName('back');
+    var front = document.getElementsByClassName('front');
+
+    for (var i = 0; i < back.length; i++) {
+        back[i].style.display = "block";
+        front[i].style.display = "none";
+    }
+
+};
